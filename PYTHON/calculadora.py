e19fbea7167bd59d32290b674f2f72ef5b929957
diegogarciia <127989567+diegogarciia@users.noreply.github.com>
2025-09-10 20:14:33 +0200

    Delete PYTHON directory

--- PYTHON/calculadora.py
+++
@@ -1,7 +0,0 @@
-def sumar():
-    num1 = float(input("Dame el primer número"))
-    print()
-    num2 = float(input("Dame el segundo número"))
-    print()
-    resultado = num1 + num2
-    print(resultado)
