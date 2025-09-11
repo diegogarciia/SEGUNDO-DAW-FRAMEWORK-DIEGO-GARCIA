228133cdd8ee61cbd79b7cebcf5782cf7966f715
diegogarciia <127989567+diegogarciia@users.noreply.github.com>
2025-09-11 18:46:11 +0200

    ejercicio 3

---
+++ PYTHON/EJERCICIOS/Ejercicio 3 Clasificacion de goleadores.py
@@ -0,0 +1,18 @@
+goles = [22, 12, 19, 5, 40, 16]
+
+cantidadjugadores = len(goles)
+
+print("Hay " + cantidadjugadores.__str__() + " jugadores")
+
+goles.insert(1, 8)
+
+print("Lista de goles ordenada de menos a mayor:")
+goles.sort()
+print(goles)
+print("Lista de goles ordenada de mayor a menos:")
+goles.reverse()
+print(goles)
+
+print("El máximo goleador metió la cantidad de " + max(goles).__str__() + " goles")
+print("La cantidad de goles que metió el jugador menos goleador es de " + min(goles).__str__() + " goles")
+
